801dad35251843c7c159cfc0a5ce0210cc24d673
Eugene Boguslavsky <eugene@mystenlabs.com>
2025-08-28 13:39:25 -0700

    ci: Revert "chore: set version bump pr to auto-merge" (#2485)
    Revert "chore: set version bump pr to auto-merge (#2482)"
    
    This reverts commit 4e7d3afe5cfd08b2f662fb69a118c1c45d9c1436.

--- scripts/bump_sui_testnet_version.sh
+++ scripts/bump_sui_testnet_version.sh
@@ -113,7 +113,4 @@ PR_URL=$(gh pr create \
   --body "$BODY" \
   2>&1 | grep -Eo 'https://github.com/[^ ]+')
 
-# Setting the PR to auto merge
-gh pr merge --auto --squash --delete-branch "$BRANCH"
-
 echo "Pull request created: $PR_URL"
